437c779e1203875f8bd2ef35c1e00350ab54b5d3
Edward Kelly <eddie@sektek.net>
2025-11-02 15:11:46 +0000

    feat: replace job-name-builder with job-name-provider for improved type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export * from './bull-mq-service-options.js';
 export * from './job-event-extractor.js';
-export * from './job-name-builder.js';
+export * from './job-name-provider.js';
 export * from './jobs-options-provider.js';
---
+++ src/types/job-name-provider.ts
@@ -0,0 +1,15 @@
+import {
+  Event,
+  EventBasedStringProvider,
+  EventBasedStringProviderComponent,
+  EventBasedStringProviderFn,
+} from '@sektek/synaptik';
+
+export type JobNameProviderFn<T extends Event = Event> =
+  EventBasedStringProviderFn<T>;
+
+export interface JobNameProvider<T extends Event = Event>
+  extends EventBasedStringProvider<T> {}
+
+export type JobNameProviderComponent<T extends Event = Event> =
+  EventBasedStringProviderComponent<T>;
