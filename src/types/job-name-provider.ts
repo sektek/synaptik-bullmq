5dabdd68b9d4d93847da05ee814b7bffbe8d7b66
Edward Kelly <eddie@sektek.net>
2026-03-07 14:02:44 +0000

    chore: lint fix

--- src/types/job-name-provider.ts
+++ src/types/job-name-provider.ts
@@ -8,8 +8,9 @@ import {
 export type JobNameProviderFn<T extends Event = Event> =
   EventBasedStringProviderFn<T>;
 
-export interface JobNameProvider<T extends Event = Event>
-  extends EventBasedStringProvider<T> {}
+export interface JobNameProvider<
+  T extends Event = Event,
+> extends EventBasedStringProvider<T> {}
 
 export type JobNameProviderComponent<T extends Event = Event> =
   EventBasedStringProviderComponent<T>;
